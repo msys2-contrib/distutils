afd18552cea4798af11414e53a516f5a0dca57f5
Christoph Reiter <reiter.christoph@gmail.com>
2022-03-20 20:54:49 +0100

    drop unixcompiler changed
    not relevant in our case

--- distutils/unixccompiler.py
+++ distutils/unixccompiler.py
@@ -137,7 +137,7 @@ class UnixCCompiler(CCompiler):
     xcode_stub_lib_extension = ".tbd"
     static_lib_format = shared_lib_format = dylib_lib_format = "lib%s%s"
     xcode_stub_lib_format = dylib_lib_format
-    if sys.platform == "cygwin" or sys.platform == "win32":
+    if sys.platform == "cygwin":
         exe_extension = ".exe"
 
     def preprocess(self, source, output_file=None, macros=None,
